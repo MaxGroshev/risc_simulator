98c8104a4e5c933c1646cd195507c3dd224e67c4
ArsenySamoylov <samoylov.arseny@gmail.com>
2025-12-08 20:05:21 +0300

    Tests: virtual queen

--- e2e_tests/virtual_memory.c
+++ e2e_tests/virtual_memory.c
@@ -85,11 +85,56 @@ void enable_vm(uint64_t root_page_table)
 }
 
 
-int test_function()
-{
-    return 12345;
+
+// Queens
+void print_u64(unsigned long x) {
+    register volatile unsigned long a0 asm("a0") = x;
+    register volatile unsigned long a7 asm("a7") = 1;
+    asm volatile ("ecall");
 }
 
+int is_safe(long row, long col, long* board) {
+    // print_u64(0xBEAF);
+    for (long i = 0; i < row; i++) {
+        long p = board[i];
+        if (p == col || p - i == col - row || p + i == col + row) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void solve(long row, long* board, unsigned long* count) {
+    // print_u64(0xDEAD);
+    if (row == 11) {
+        (*count)++;
+        // print_u64(*count);
+        return;
+    }
+    // print_u64(0xDEAD);
+
+    for (long long col = 0; col < 11; col++) 
+    {
+        // print_u64(0xBEAF);
+        if (is_safe(row, col, board)) {
+            board[row] = col;
+            solve(row + 1, board, count);
+        }
+    }
+
+    // print_u64(0xDEAD);
+}
+
+int main() {
+    long board[11] = {0};
+    unsigned long solution_count = 0;
+
+    solve(0, board, &solution_count);
+
+    print_u64(solution_count);
+
+    return solution_count;  // 92
+}
 
 void _start(void)
 {
@@ -97,7 +142,7 @@ void _start(void)
 
     enable_vm((uint64_t)l1_page_table);
 
-    int result = test_function();
+    int result = main();
 
     asm volatile ("mv a0, %0" : : "r"(result)); 
     register volatile unsigned long a7 asm("a7") = 93;
